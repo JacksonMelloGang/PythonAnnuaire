0bee2623e2ac04841c2d77ccd1cf502e00c4356b
JacksonMellogang <50780747+JacksonMelloGang@users.noreply.github.com>
2024-01-20 01:52:53 +0100

    fixing issues

--- client/client.py
+++ client/client.py
@@ -1,10 +1,23 @@
 import socket
 import json
 import time
+import os
 
-username = ""
-password = ""
-logged_in = False
+# Constants, used to identify the type of request: (Directory = Annuaire)
+CONNEXION_TYPE = "CONNEXION" # Provided => {"username": "username", "password": "password"}
+DISCONNECT_TYPE = "DISCONNECT" # Provided => {}
+DATA_REQUEST_TYPE = "DATA_REQUEST" # Provided => {"token": token, "data": {"annuaire": "directory_name"}}
+ADD_CONTACT_TYPE = "ADD_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+EDIT_CONTACT_TYPE = "EDIT_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+REMOVE_CONTACT_TYPE = "REMOVE_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+SEARCH_CONTACT_TYPE = "SEARCH_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+LIST_DIRECTORIES = "LIST_DIRECTORIES" # Provided => {"token": token, "data": {}}
+
+
+ADD_USER_TYPE = "ADD_USER" # Provided => {"token": token, "data": {"username": "username", "password": "password"}}
+REMOVE_USER_TYPE = "REMOVE_USER" # Provided => {"token": token, "data": {"username": "username", "password": "password"}}
+EDIT_USER_TYPE = "EDIT_USER" # Provided => {"token": token, "data": {"username": "username", "password": "password"}}
+LIST_USERS_TYPE = "LIST_USERS" # Provided => {"token": token, "data": {}}   
 
 class Client:
 
@@ -44,30 +57,36 @@ class Client:
 
         content = self.socket.recv(1024).decode('utf-8')
         try:
-            json_content = json.load(content)
+            json_content = json.loads(content)
         except(Exception):
-            print(f"Couldn't Convert Received Data {content}")
+            print(f"Couldn't convert received data:\n{content}")
             
         return json_content
 
     def login(self, username, password):
+        # Check if we opened a channel with the server
         if(self.connected == False):
             print("You are not logged to the server")
             return self.user_connected
 
+        # Check if we are already logged in
         if(self.user_connected == True):
             print("You are already logged in")
             return self.user_connected
 
         self.convert_and_transmit_data("CONNEXION", {"username": username, "password": password})
-        response = self.receive_and_convert_data()
-        if(response[type] == "CONNEXION_OK"):
-            print("Identifiant Vérifié, Bienvenue.")
+        response = self.receive_and_convert_data() # get the response from the server
 
-            self.token = response["token"]
+        # if we get a response and type response["type"] exists and is equal to CONNEXION_OK, we validate the login :)
+        if(response and "type" in response and response["type"] == "CONNEXION_OK"):
+            print("Identifiant Vérifié, Bienvenue.")
+            time.sleep(1)
+            clear()
+
+            self.token = response["data"]["token"]
             self.user_connected = True
         else:
-            print("Identifiant invalide !")
+            print(f"An Unknow error occured : {response['data']['message']}")
 
         return self.user_connected
 
@@ -105,16 +124,11 @@ class Client:
 ##############################################################################################################################
 ##############################################################################################################################
 ##############################################################################################################################
-    
-def ask_username():
-    while(username == ""):
-        username = input("Username: ")
 
-def ask_password():
-    while(password == ""):
-        password = input("Password: ")
 
 def ask_menu_choice():
+    min_max = (1, 5)
+
     print("1. Afficher mon annuaire")
     print("2. Ajouter un contact")
     print("3. Supprimer un contact")
@@ -122,6 +136,13 @@ def ask_menu_choice():
     print("5. Quitter")
 
     choice = input("Choice: ")
+
+    # While choice is not a valid number or is not in the range of min_max
+    while(choice.isdigit() == False or int(choice) < min_max[0] or int(choice) > min_max[1]):
+        print("Invalid Choice")
+        ask_menu_choice()
+
+    # Choice validated
     return choice
 
 def ask_contact_information():
@@ -138,24 +159,37 @@ def ask_annuaire_contacts(client, annuaire_name):
     client.convert_and_transmit_data("DATA_REQUEST", {"token": client.get_token(), "data": {"annaire": annuaire_name}})
 
 def main():
+    # Global Variables
+    username = ""
+    password = ""
+    logged_in = False
+
+    
     client = Client()
     client.establish_connection("localhost", 5555)
 
     if(client.get_connection_status() == False):
         print("Couldn't connect to the server")
         return
-
+    else:
+        print("Successfully connected to the server")
 
     # Connected to the server, now asking for credentials
     while(logged_in == False):
-        ask_username()
-        ask_password()
+        while(username == ""):
+            username = input("Username: ")
+
+        while(password == ""):
+            password = input("Password: ")
 
         logged_in = client.login(username, password)
 
         if(logged_in == False):
             print("Couldn't Log In, Try Again")
             return
+        else:
+            print("Logged In Successfully")
+            logged_in = True
 
 
     # Logged in, now displaying the menu and asking for user input    
--- server/server.py
+++ server/server.py
@@ -8,23 +8,77 @@ import secrets
 user_folder = "./user_files"
 valid_token = {}
 
+# Received Constants, used to identify the type of request: (Directory = Annuaire)
+CONNEXION_TYPE = "CONNEXION" # Provided => {"username": "username", "password": "password"}
+DISCONNECT_TYPE = "DISCONNECT" # Provided => {}
+DATA_REQUEST_TYPE = "DATA_REQUEST" # Provided => {"token": token, "data": {"annuaire": "directory_name"}}
+ADD_CONTACT_TYPE = "ADD_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+EDIT_CONTACT_TYPE = "EDIT_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+REMOVE_CONTACT_TYPE = "REMOVE_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+SEARCH_CONTACT_TYPE = "SEARCH_CONTACT" # Provided => {"token": token, "data": {"annuaire": "directory_name", "contact": {"name": "name", "first_name": "first_name", "phone": "phone", "email": "email"}}}
+LIST_DIRECTORIES = "LIST_DIRECTORIES" # Provided => {"token": token, "data": {}}
+
+
+ADD_USER_TYPE = "ADD_USER" # Provided => {"token": token, "data": {"username": "username", "password": "password"}}
+REMOVE_USER_TYPE = "REMOVE_USER" # Provided => {"token": token, "data": {"username": "username", "password": "password"}}
+EDIT_USER_TYPE = "EDIT_USER" # Provided => {"token": token, "data": {"username": "username", "password": "password"}}
+LIST_USERS_TYPE = "LIST_USERS" # Provided => {"token": token, "data": {}}   
+
+# Sent Constants, used to identify the type of request: (Directory = Annuaire)
+CONNEXION_OK_TYPE = "CONNEXION_OK" # Provided => {"message": "Valid Credentials", "token": token}
+DISCONNECT_OK_TYPE = "DISCONNECT_OK" # Provided => {"message": "Disconnected Successfully"}
+ERROR_TYPE = "ERROR" # Provided => {"message": "Error Message"}
+
+def convert_and_transmit_data(self, request_type, data):
+    if(self.connected == False):
+        print("You are not logged to the server")
+        return
+
+    try:
+        request = {"type": request_type, "data": data}            
+        self.socket.send(json.dumps(request).encode('utf-8'))
+    except(Exception):
+        print("An Unknow Error Has Occured while sending request")
+    print("Sent !")
+
+def receive_and_convert_data(self):
+    json_content = False
+
+    if(self.connected == False):
+        print("You are not logged to the server")
+        return
+
+    content = self.socket.recv(1024).decode('utf-8')
+    try:
+        json_content = json.loads(content)
+    except(Exception):
+        print(f"Couldn't convert received data:\n{content}")
+    return json_content
+
 def handle_login(client_socket, data):
+    # check if username and password are in data
+    if "username" not in data or "password" not in data:
+        client_socket.send(json.dumps({"type": "ERROR", "data": {"message": "Missing Credentials"}}).encode('utf-8'))
+        return
+
     # Logique de gestion de la connexion
-    username = data["login"]
+    username = data["username"]
     password = data["password"]
 
     result, token = authenticate_user(username, password)
 
     # Vérification des identifiants - À adapter en fonction de votre logique d'authentification
     if result == True:
-        client_socket.send(json.dumps({"type": "CONNEXION_OK", "data": {"message": "Valid Credentials.", "token": token}})).encode('utf-8')
+        message = json.dumps({"type": "CONNEXION_OK", "data": {"message": "Valid Credentials", "token": token}})
+        client_socket.send(message.encode('utf-8'))
     else:
         client_socket.send(json.dumps({"type": "ERROR", "data": {"message": "Invalid Credentials"}}).encode('utf-8'))
 
 def authenticate_user(username, password):
-    # create./user_files folder if it doesn't exist
+    # create./user_files folder if it doesn't exist where is the program is executed    
     if not os.path.exists(user_folder):
         os.makedirs(user_folder)
+        return False, None
 
     # get name of every folder in ./user_files
     user_folders = os.listdir(user_folder)
@@ -47,9 +101,6 @@ def authenticate_user(username, password):
             user_info_file.write(password)
         return False, None
     
-        
-
-
 def handle_disconnect(client_socket):
     # close the connection & invalid token
     client_socket.close()
@@ -87,6 +138,7 @@ def handle_client(client_socket):
             print(f"Invalid Data Type from {client_socket.getpeername()} \nReceived Data Type: {request_type}\nFull Data:\n {json_data}")
             return
 
+        # match is equivalent of switch case in python
         match(request_type):
             case "CONNEXION":
                 handle_login(client_socket, json_data["data"])
@@ -94,8 +146,9 @@ def handle_client(client_socket):
                 handle_disconnect(client_socket)
             case "DATA_REQUEST":
                 handle_data_request(client_socket, json_data)
-            case _:
-                client_socket.send(json.dumps({"type": "ERROR", "data": {"message": "Requête non valide"}}).encode('utf-8'))
+            
+            case _: # default case
+                client_socket.send(json.dumps({"type": "ERROR", "data": {"message": "Invalid Request"}}).encode('utf-8'))
 
         # Traitement des données (dans cet exemple, simplement les renvoyer)
         client_socket.send(data)
