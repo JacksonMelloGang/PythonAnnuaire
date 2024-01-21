92327ca74f18cd947cc0ca09765b703c3f66cbd1
JacksonMellogang <50780747+JacksonMelloGang@users.noreply.github.com>
2024-01-21 18:17:51 +0100

    PERMISSIONS SYSTEM WORKING SORTED FILES AND FUNCTIONS

---
+++ client/handler/user/case_search_contact.py
@@ -0,0 +1,41 @@
+import time
+
+from client.utils.constants import SEARCH_CONTACT_TYPE, RESPONSE_OK_TYPE
+from client.utils.contact.ask_contact import ask_server_annuaire_contacts
+from client.utils.display.list_contacts import list_contacts
+from client.utils.utils import clear
+
+
+def case_search_contact(annuaire_name, client):
+    # Rechercher un contact
+    contacts = ask_server_annuaire_contacts(client, annuaire_name)
+    if (contacts == False):
+        print("Vous n'avez aucun contact dans votre annuaire")
+        time.sleep(1)
+        clear()
+        return
+
+    print("Voici les contacts dans votre annuaire:")
+    list_contacts(contacts)
+
+    word_to_search = input("Veuillez entrer le nom du contact à rechercher: ").strip()
+
+    # send request to server
+    client.convert_and_transmit_data(SEARCH_CONTACT_TYPE,
+                                     {"token": client.get_token(), "username": client.get_username(),
+                                      "annuaire_name": annuaire_name, "word_to_search": word_to_search})
+    response = client.receive_and_convert_data()
+
+    if (response["type"] == RESPONSE_OK_TYPE):
+        if ("contacts" in response["data"]):
+            clear()
+            print("Voici les contacts trouvés:")
+            list_contacts(response["data"]["contacts"])
+            print("=====================================")
+        else:
+            print("Aucun contact trouvé")
+    else:
+        print("Une erreur est survenue lors de la recherche du contact")
+        print(response["data"]["message"])
+        time.sleep(3)
+        clear()
--- server/handler/user/directory/look_into_directory.py
+++ server/handler/user/directory/look_into_directory.py
@@ -1,12 +1,18 @@
+from constants import USER_FOLDER, ERROR_TYPE, RESPONSE_OK_TYPE
+from utils import read_files, convert_and_transmit_data
+
+import os
+
 def handle_look_directory_request(client_socket, data):
     annuaire_file_name = data["data"]["annuaire_content"]
 
     # Access folder of asked user and check if {user}_annuaire.txt exists, if yes, read it and send it back
     user_folder_name = annuaire_file_name.split("_")[0]
+    annuaire_path = os.path.join(USER_FOLDER, user_folder_name, f"{annuaire_file_name}_annuaire.txt")
 
-    annuaire_content = read_files(f"{USER_FOLDER}/{user_folder_name}/{annuaire_file_name}.txt")
+    annuaire_content = read_files(annuaire_path)
 
-    if annuaire_content is  False:
+    if annuaire_content is False:
         convert_and_transmit_data(client_socket, ERROR_TYPE, {"message": f"{user_folder_name}_annuaire.txt Not Found in {USER_FOLDER}/{user_folder_name}"})
         return
 
